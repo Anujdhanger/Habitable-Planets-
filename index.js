da2ac8fead8bcd1657a8ad1337647ae7a513a1be
Anujdhanger <98631468+Anujdhanger@users.noreply.github.com>
2024-05-26 15:39:44 +0530

    ✨ docs: Add comments to improve code readability

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//importing modules
 const fs = require('fs');
 const {parse} = require('csv-parse');
 
 const result=[];
-
+//function to find if planet is habitable or not
 function ishabitable(planet){
     if((planet['koi_disposition']=='CONFIRMED')&&planet['koi_insol']>0.36&&planet['koi_insol']<1.11&&planet['koi_prad']<1.6) return true;
     return false;
 }
+//reading csv data in stream form and then transforming it to javascript object using csv-parse module
 const readablestream=fs.createReadStream('Kepler_data.csv');
+//using pipe to continuously pass every readable stream so that it can be transformed
 const parser=readablestream.pipe(parse({
     comment: '#',
     columns: true,
